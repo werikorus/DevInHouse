db0c495471f83642a6b1bee1b1ca82c00d4aeb73
Werik Santos <werik.msti@gmail.com>
2021-11-02 20:59:29 -0300

    ex09

--- Semana 01/[M1S02] - Ex 9 - Construa uma calculadora/scripts/script.js
+++ Semana 01/[M1S02] - Ex 9 - Construa uma calculadora/scripts/script.js
@@ -1,8 +1,21 @@
 function AskingUser(){
-  let op = prompt('Informe um tipo de cálculo: \n +  -  *  /');
 
+  var operations = ['+','-','*','/'];
+  var validOperation = false;
+
+  // primeiro, validamos o tipo de operação: 
+  while(!validOperation){
+    var op = prompt('Informe um tipo de cálculo: \n +  -  *  /');
+    if(operations.includes(op)){
+      validOperation = true;
+    }else{
+      alert('Operação inválida!');
+      validOperation = false;
+    }
+  };
+
+  // válida a operação, agora vamos validar os números:
   var validNumber = false;
-
   while(!validNumber){
     var value1 = prompt('Informe o primeiro valor a ser calculado: ');
     if(!value1||isNaN(value1) || ((op=='/') && value1==0)){
@@ -24,11 +37,14 @@ function AskingUser(){
     
   };
   
+  // e por fim, fazer o cálculo de acordo com a operação e os números:
   let result = eval(`${value1} ${op} ${value2}`);
+  
 
+  // feito os cálculos, mostramos ao usuário o resultado;
   let msg = `Operação: ${op} \n Primeiro valor: ${value1}\n Segundo valor: ${value2}\n`;
   msg += `O resultado do seu cálculo é: ${result}`;
-  alert(msg);
 
+  alert(msg);
   document.getElementById('paragrafo').innerText = msg;
 };
