d92d13cedfe20e1d78292761750d78ef09bdcaf8
Werik Santos <werik.msti@gmail.com>
2021-11-03 22:27:29 -0300

    exe10

---
+++ Semana 01/[M1S02] - Ex 10 - Alerte as horas para o usuário/scripts/script.js
@@ -0,0 +1,28 @@
+var paragrafo = '';
+var atualHour='';
+
+function GetAtualHours(){
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+
+  atualHour = `${hour}:${minute}`;
+  console.log(atualHour);
+  paragrafo.innerHTML = '';
+  
+  let msg = `\nHora atual: `;
+  paragrafo = document.getElementById('paragrafo');
+  paragrafo.innerText += msg;
+  paragrafo.innerHTML += `<strong>${atualHour}</strong>`;
+
+  return atualHour;
+};
+
+function ShowHoursToUser(){
+  const atualHour = GetAtualHours();  
+  alert(`Bem vindo!\nHora atual: ${atualHour}`);
+};
+
+function checkSate(){
+  cron = setInterval(()=>{GetAtualHours()}, 1000);
+};
