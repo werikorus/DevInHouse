787cfc3a0b7f6e63580658d9ed6dc511404ce189
Werik Santos <werik.msti@gmail.com>
2021-11-05 22:34:55 -0300

    correcoes

--- Semana 03/[M1S03] - Ex 1 - Calcule automaticamente a idade/scripts/script.js
+++ Semana 03/[M1S03] - Ex 1 - Calcule automaticamente a idade/scripts/script.js
@@ -1,14 +1,17 @@
-function CalcPA(){
-  var number1 = parseInt(prompt('Valor inicial: '));
-  var raiz = parseInt(prompt('Raíz: '));
-  var msg = 'P.A = ';
-  var total = number1; 
+function CalcAge(){
+  let today = Date.parse(new Date());
+  let birth = Date.parse(document.getElementById('bornDate').value);
+  let difInTicks   = Math.abs(today-birth);
+  let convertInDays = Math.ceil(difInTicks/(1000*60*60*24));
+  let age = (convertInDays/365.25).toFixed();
 
-  for(i=1; i<=10; i++){
-    msg += ` ${total},`;
-    total += raiz;
+  if(isNaN(age)){
+    alert('Para calcular, informe uma data válida!');
+  }else{
+    let view = document.getElementById('rec');
+    let paragrafo = document.getElementById('paragrafo');
+    paragrafo.innerText = '';
+    view.innerHTML  = `Sua idade é: <strong>${age}</strong>\n`;
+    view.innerHTML += '<button> <a href="/"  style="text-decoration:none;">voltar </a></button>'
   };
-
-  let paragrafo = document.getElementById('paragrafo');
-  paragrafo.innerText = `Valor inicial: ${number1}\n Raiz: ${raiz}\n${msg}`;
-};
+}; 
