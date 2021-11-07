edb6f69d7c8127433f46e67a39cfc2da31216b72
Werik Santos <werik.msti@gmail.com>
2021-11-07 11:51:55 -0300

    correcoes 03

--- Semana 03/[M1S03] - Ex 2 - Construa uma calculadora simples/scripts/script.js
+++ Semana 03/[M1S03] - Ex 2 - Construa uma calculadora simples/scripts/script.js
@@ -3,12 +3,11 @@ function Calc(button){
   const value2 = parseInt(document.getElementById('n2').value);
   var op = button.value;
 
-  if(isNaN(value1)||isNaN(value2)){
-    alert('Para calcular, informe números válidos!');
-  }else{
+  if(!(isNaN(value1)||isNaN(value2))){
     var total = eval(`${value1}${op}${value2}`);
     var viewTotal = document.getElementById('totalCalc');
     viewTotal.value = `${total}`;
-    console.log(total);
+  }else{
+    alert('Para calcular, informe números válidos!');
   };
 }; 
