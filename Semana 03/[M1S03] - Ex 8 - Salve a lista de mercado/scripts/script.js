2012b590934fb17e8c2df6720104343d43788d8d
Werik Santos <werik.msti@gmail.com>
2021-11-21 12:51:53 -0300

    finalizado exercicio 8

--- Semana 03/[M1S03] - Ex 8 - Salve a lista de mercado/scripts/script.js
+++ Semana 03/[M1S03] - Ex 8 - Salve a lista de mercado/scripts/script.js
@@ -1,17 +1,37 @@
+var Lista = [];
+var Input = document.getElementById('textInput');
+
 function InsertItem(){
   const description = document.getElementById('textInput').value;
 
   if(description.length!=0){ 
-  const item = document.createElement("OPTION");
-  const text = document.createTextNode(description);
-        item.appendChild(text);
-        
-  console.log(description.length);
+    const item = document.createElement("OPTION");
+    const text = document.createTextNode(description);
+          item.appendChild(text);
 
-  let select = document.getElementById('lista');
-      select.appendChild(item);
+    console.log(description.length);
+
+    let select = document.getElementById('lista');
+        select.appendChild(item);
+  
+    Input.innerText = '';
+
   }else{
      alert('Informe uma item para sua lista');
      const input = document.getElementById('textInput').focus();  
+  };
+
+  Lista.push(`${Lista.length+1} - ${description}`);
+  console.log(Lista);
+
+};
+
+function SalvarLista(){
+  if(Lista.length != undefined ||Lista != []){
+    localStorage.setItem('lista',JSON.stringify(Lista)); 
+    alert('Lista salva com sucesso!');
+  }else{
+    alert('Ainda não há itens na sua lista!');
   }
+
 };
