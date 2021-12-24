3dfbd00e4d6900d06796e029dd4f2a3d75727956
Werik Santos <werik.msti@gmail.com>
2021-12-23 21:54:49 -0300

    Exercício 07  semana 05

---
+++ Semana 05/[M1S05] - Ex 7 - Valide o CPF em Cliente/scripts/Conta.js
@@ -0,0 +1,40 @@
+import { Cliente } from "./Cliente";
+export class Conta {
+  get numeroDaConta() {
+    return this._numeroDaConta;
+  }
+  set numeroDaConta(value) {
+    this._numeroDaConta = value;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+  set saldo(value) {
+    this._saldo = value;
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+  set cliente(value) {
+    this._cliente = value;
+  }
+
+  constructor (numeroDaConta, saldo, cliente){
+    try{
+
+      if(!typeof(numeroDaConta!=="string")||
+         !typeof(saldo!=="number")||
+         !(cliente instanceof Cliente)
+      )new Error("Dados da conta são inválidos!");
+
+    }catch(e){
+      alert(e.message);
+    }
+
+    this._numeroDaConta = numeroDaConta;
+    this._saldo = saldo;
+    this._cliente = new Cliente();
+  };
+}
