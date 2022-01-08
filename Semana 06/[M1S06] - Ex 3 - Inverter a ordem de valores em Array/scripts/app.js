d45a5ac25b88f350c8139760714b075c49dda6d8
Werik Santos <werik.msti@gmail.com>
2022-01-07 22:00:56 -0300

    inicio ex 3

---
+++ Semana 06/[M1S06] - Ex 3 - Inverter a ordem de valores em Array/scripts/app.js
@@ -0,0 +1,67 @@
+import { Endereco } from './Endereco.js';
+import { Cliente } from './Cliente.js';
+
+const mensagemOla = (nome) =>{
+      if(nome!=''){
+        alert('Olá, ' + nome);
+      }else{
+        alert('Bem vindo usuário!');
+      }
+};
+    
+window.onload = mensagemOla('Werik');
+
+
+const arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayInvertido = [];
+
+const myForm = document.getElementById('myForm');
+      myForm.addEventListener('submit', saveCustomer);
+
+const inverteNumeros = () => {
+  
+
+  for(var i=0; i<=arrayNumeros.length; i++){
+    arrayInvertido.push(arrayNumeros);
+  };
+
+  console.log(arrayNumeros);
+  console.log(arrayInvertido);
+};
+
+window.onload = inverteNumeros();
+
+function saveCustomer(event){
+  event.preventDefault();
+
+    const formData = new FormData(myForm);
+
+    const nomeCliente = formData.get('nome');
+    const cpfCliente =  formData.get('cpf');
+    const numeroCelularCliente = formData.get('numeroCelular');
+     
+    const logradouro = formData.get('logradouro');
+    const numero = formData.get('numero');
+    const cidade = formData.get('cidade');
+    const estado = formData.get('estado');
+    const pais = formData.get('pais');
+    const cep = formData.get('cep');
+
+    const address = new Endereco(logradouro, numero, cidade, estado, pais, cep);
+    const newCustomer = new Cliente(nomeCliente, cpfCliente, numeroCelularCliente, address);
+
+    const p = document.getElementById('paragrafo');
+          p.innerHTML = '<strong>Cadastro salvo com sucesso!</strong>'
+
+    myForm.innerText = `Nome:${newCustomer.nome}
+                        Cpf:${newCustomer.cpf}
+                        Número celular:${newCustomer.numeroCelular}
+                        Rua: ${newCustomer.endereco.logradouro}
+                        Número: ${newCustomer.endereco.numero}
+                        Cidade: ${newCustomer.endereco.cidade}
+                        Estado: ${newCustomer.endereco.estado}
+                        País: ${newCustomer.endereco.pais}
+                        Cep: ${newCustomer.endereco.cep}`;
+};
+
+
