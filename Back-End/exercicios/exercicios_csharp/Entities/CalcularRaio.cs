44374255b04b7472f43d86f3f245e1934e688ac6
Werik Santos <werik.msti@gmail.com>
2022-02-27 20:55:41 -0300

    exercicio 01

---
+++ Back-End/exercicios/exercicios_csharp/Entities/CalcularRaio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace exercicios_csharp.Entities
+{
+    class CalcularRaio
+    {
+        public double Raio { get; set; }
+
+        public CalcularRaio(double raio)
+        {
+            this.Raio = raio;
+        }
+        public CalcularRaio()
+        {
+            
+        }
+
+        public double CalcularRaioCirculo()
+        {
+            double pi = 3.14159;
+            double total = pi * (this.Raio * this.Raio);
+
+            return total;
+        }
+        
+    }
+}
---
+++ Back-End/exercicios/exercicios_csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using exercicios_csharp.Entities;
+
+namespace exercicios_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o raio: ");
+            double raio = float.Parse(Console.ReadLine());
+
+            CalcularRaio calcular = new CalcularRaio(raio);
+
+            double total = calcular.CalcularRaioCirculo();
+
+
+            Console.WriteLine($"O total da área é : {total}");
+
+        }
+    }
+}
+
