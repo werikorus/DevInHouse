70a55d8beb20f0cc2b9b9d89bd6c064703a2578a
Werik Santos <werik.msti@gmail.com>
2022-02-27 22:10:02 -0300

    [M2S02] Ex 3 - Busca número maior

---
+++ Back-End/exercicios/exercicios_csharp/Entities/QualMaiorNumero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercicios_csharp.Entities
+{
+    class QualMaiorNumero
+    {
+        public List<int> ListaNumeros { get; set; }
+
+        public QualMaiorNumero(List<int> listaNumeros)
+        {
+            ListaNumeros = listaNumeros;
+        }
+
+        public QualMaiorNumero()
+        {
+
+        }
+
+        public int AcharMaiorNumero()
+        {
+            return this.ListaNumeros.Max();
+        }        
+    }
+}
--- Back-End/exercicios/exercicios_csharp/Program.cs
+++ Back-End/exercicios/exercicios_csharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using exercicios_csharp.Entities;
+using System.Collections.Generic;
 
 namespace exercicios_csharp
 {
@@ -7,16 +8,20 @@ namespace exercicios_csharp
     {
         static void Main(string[] args)
         {
-            Console.Write("Informe o raio: ");
-            double raio = float.Parse(Console.ReadLine());
+            Console.WriteLine("Informe 5 números: ");
 
-            CalcularRaio calcular = new CalcularRaio(raio);
+            List<int> numeros = new List<int>();
 
-            double total = calcular.CalcularRaioCirculo();
+            for(var i=0; i<=5; i++)
+            {
+                numeros.Add(int.Parse(Console.ReadLine()));
+            }
 
+            QualMaiorNumero MaiorNumero = new QualMaiorNumero(numeros);
 
-            Console.WriteLine($"O total da área é : {total}");
+            int MaxNumber = MaiorNumero.AcharMaiorNumero();
 
+            Console.WriteLine($"O maior número é: {MaxNumber}");
         }
     }
 }
