4bf433326c445df8eb56d28505604f5173174f66
Werik Santos <werik.msti@gmail.com>
2022-02-27 21:31:34 -0300

    exercicio 02

---
+++ Back-End/exercicios/exercicios_csharp/Entities/CalcularTemperatura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios_csharp.Entities
+{
+    class CalcularTemperatura
+    {
+        public double TempCelcius { get; set; }
+
+        public CalcularTemperatura(double tempCelcius)
+        {
+            this.TempCelcius = tempCelcius;
+        }
+
+        public CalcularTemperatura()
+        {
+            
+        }
+
+
+        public double CalcTemperatura()
+        {
+            double Fahrenheit = this.TempCelcius * 1.8 + 32;
+            return Fahrenheit;
+        }
+    }
+}
