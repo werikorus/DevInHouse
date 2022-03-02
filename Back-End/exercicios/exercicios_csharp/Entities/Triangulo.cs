5e1efd16c2bf57ab8c8dd766269b63b155bfec80
Werik Santos <werik.msti@gmail.com>
2022-03-02 20:53:46 -0300

    [M2S02] Ex 6 - Equilátero, isósceles ou escaleno

---
+++ Back-End/exercicios/exercicios_csharp/Entities/Triangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios_csharp.Entities
+{
+    class Triangulo
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangulo()
+        {
+
+        }
+
+        public Triangulo(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+
+        public Boolean VerificarTriangulo()
+        {
+            if ((A + B) < C)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public string TipoTriangulo()
+        {
+            string tipoTriangulo;
+
+            if (A == B && A == C && B == C)
+            {
+                tipoTriangulo = "Equilátero";
+            }
+            else if (A == B || A == C || B == C)
+            {
+                tipoTriangulo = "Isósceles";
+            }
+            else
+                tipoTriangulo = "Escaleno";
+
+            return tipoTriangulo;
+        }
+
+    }
+}
--- Back-End/exercicios/exercicios_csharp/Program.cs
+++ Back-End/exercicios/exercicios_csharp/Program.cs
@@ -8,15 +8,28 @@ namespace exercicios_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe 10 números: ");
-            List<int> numeros = new List<int>();
-            for(var i=1; i<=10; i++)
+            Console.WriteLine("Informe 3 lados de um triângulo!");
+            Console.Write("Lado A: ");
+            double A = double.Parse(Console.ReadLine());
+            
+            Console.Write("Lado B: ");
+            double B = double.Parse(Console.ReadLine());
+
+            Console.Write("Lado C: ");
+            double C = double.Parse(Console.ReadLine());
+
+            Triangulo triangulo = new(A, B, C);
+
+            if (!triangulo.VerificarTriangulo())
             {
-                numeros.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Não pode ser um triângulo!");
             }
-            Ordenacao Ordenacao = new Ordenacao(numeros);
-            Console.WriteLine($"Números ordenados descrescente: ");
-            Ordenacao.OrdenarNumeros();
+            else
+            {
+                string tipoTriangulo = triangulo.TipoTriangulo();
+                Console.WriteLine($"O tipo do triângulo é: {tipoTriangulo}");
+            }
+
         }
     }
 }
