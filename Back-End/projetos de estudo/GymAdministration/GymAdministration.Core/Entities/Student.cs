148eea72d19bde85b22326a5819d0bf260da9ddd
Werik Santos <werik.msti@gmail.com>
2022-04-03 12:50:35 -0300

    [M3S03] Ex 03 - Criar a Classe Student

---
+++ Back-End/projetos de estudo/GymAdministration/GymAdministration.Core/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymAdministration.Core.Entities;
+
+namespace GymAdministration.Core.Entities
+{
+    class Student
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string MedicalObservation { get; private set; }
+        public DateTime BirthdayDate { get; private set; }
+        public StatusEnum Status { get; private set; }
+
+        public Student(int id, string name, string medicalObservation, DateTime birthdayDate, StatusEnum status)
+        {
+            Id = id;
+            Name = name;
+            MedicalObservation = medicalObservation;
+            BirthdayDate = birthdayDate;
+            Status = StatusEnum.Active;
+        }
+    }
+}
---
+++ Back-End/projetos de estudo/GymAdministration/GymAdministration.Core/Enums/StatusEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace GymAdministration.Core
+{
+    enum StatusEnum
+    {
+        Inactive = 0,
+        Active = 1
+    }
+}
+ 
