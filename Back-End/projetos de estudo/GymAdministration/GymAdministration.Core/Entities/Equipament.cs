cfd6f320913bd438e3e823a923f90789c607a7eb
Werik Santos <werik.msti@gmail.com>
2022-04-03 13:33:59 -0300

    [M303] Ex 04 - Criar a Classe Equipament

--- Back-End/projetos de estudo/GymAdministration/GymAdministration.Core/Entities/Equipament.cs
+++ Back-End/projetos de estudo/GymAdministration/GymAdministration.Core/Entities/Equipament.cs
@@ -8,5 +8,17 @@ namespace GymAdministration.Core.Entities
 {
     class Equipament
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string  Description { get; private set; }
+        public DateTime AcquiredDate { get; private set; }
+
+        public Equipament(int id, string name, string description, DateTime acquiredDate)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            AcquiredDate = acquiredDate;
+        }
     }
 }
