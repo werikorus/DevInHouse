c9a6de021f0cbbb31fdbfd9c5570b0358f9f6ffa
Werik Santos <werik.msti@gmail.com>
2022-04-03 14:29:20 -0300

    [M303] Ex 06 - Criar a Classe ActivityClass

--- Back-End/projetos de estudo/GymAdministration/GymAdministration.Core/Entities/ActivityClass.cs
+++ Back-End/projetos de estudo/GymAdministration/GymAdministration.Core/Entities/ActivityClass.cs
@@ -8,5 +8,22 @@ namespace GymAdministration.Core.Entities
 {
     class ActivityClass
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime StartedActivity { get; private set; }
+        public DateTime FinishActivity { get; set; }
+        public StatusEnum Status { get; set; }
+        public Employee Employee { get; set; }
+        public Student  Student  { get; set; }
+
+
+        public ActivityClass (string name, string description, DateTime startedActivity)
+        {
+            Name = name;
+            Description = description;
+            StartedActivity = startedActivity;
+            Status = StatusEnum.Active;
+        }
     }
 }
